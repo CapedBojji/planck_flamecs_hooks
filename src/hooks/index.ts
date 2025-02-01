994e2716b5cf3fc4efe1b900d998b5af49b68b1b
CapedBaldy <capedbaldy141@gmail.com>
2025-02-01 11:43:52 +0000

    chore: bump version to 0.0.7-rc.2 and export additional hooks in index.ts

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,6 @@
 export * from "./use-event";
 export * from "./use-throttle";
 export * from "./use-delta-time";
+export * from "./use-state";
+export * from "./use-effect";
+export * from "./use-memo";
