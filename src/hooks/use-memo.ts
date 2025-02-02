6a2f0b3d913d2d9b3b351daff9c10547f9f429bd
CapedBaldy <capedbaldy141@gmail.com>
2025-02-02 03:31:26 +0000

    chore: bump version to 0.0.7-rc.7 and update useMemo to use generic type T

--- src/hooks/use-memo.ts
+++ src/hooks/use-memo.ts
@@ -2,17 +2,17 @@ import Phantom from "@rbxts/phantom";
 import { useHookState } from "../topo";
 import { Modding } from "@flamework/core";
 
-interface Storage {
-    value: unknown;
+interface Storage<T> {
+    value: T;
     dependencies?: Array<unknown>;
 }
 
 export function useMemo<T extends unknown>(callback: Callback, dependencies?: Array<unknown>, discriminator?: unknown, key?: Modding.Caller<"uuid">): T {
     assert(key, "Attempted to use useMemo without a key.");
-    const storage = useHookState<Storage>(key, discriminator);
+    const storage = useHookState<Storage<T>>(key, discriminator);
     if (!dependencies || !storage.dependencies || !Phantom.Array.equals(storage.dependencies, dependencies)) {
         storage.value = callback();
         storage.dependencies = dependencies;
     }
-    return storage.value as T;
+    return storage.value;
 }
